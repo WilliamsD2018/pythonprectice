5ad142de556c568e6b97c3707d75420bbcf25836
WilliamsD2018 <dwilliams@sjnma.org>
2018-02-06 09:41:55 -0600

    ...

---
+++ variables.py
@@ -0,0 +1,12 @@
+print('''Hello World
+
+
+
+
+
+''')
+first_name = raw_input("What is your firt name? ")
+last_name = raw_input("What is your last name? ")
+full_name = first_name  +" "+  last_name
+print('''Welcome, '''+full_name)
+
