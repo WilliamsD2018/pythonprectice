6e1885b885f616194795b81bf6a28ced912a1030
WilliamsD2018 <dwilliams@sjnma.org>
2018-02-06 10:20:37 -0600

    ....

---
+++ temperature.py
@@ -0,0 +1,10 @@
+celcius = raw_imput("please enter a temperature in celcius (~")
+celcius = int(celcius)
+
+#ask user for temperature
+
+#compute int fehrenheit
+ferenheit = 9.0/5.0 * celcius + 32
+#display final value
+
+print(ferenheit)
--- variables.py
+++ variables.py
@@ -8,5 +8,14 @@ print('''Hello World
 first_name = raw_input("What is your firt name? ")
 last_name = raw_input("What is your last name? ")
 full_name = first_name  +" "+  last_name
+#string
 print('''Welcome, '''+full_name)
+
+number1= 500
+#integer^
+number2= 1000.0
+#floating point^
+number3 = number1 / number2
+print(numbe3)
+
 
